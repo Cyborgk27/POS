851da95a169ecce74dfafee200513a024abb1577
CyborgK27 <slenderstalin@hotmail.com>
2024-04-04 12:14:28 -0500

    Agregar archivos de proyecto.

---
+++ Infrastructure/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace POS.Infrastructure.Persistences.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //Declaración o matrícula de nuestras interfaces a nivel de repo
+        ICategoryRepository Category {  get; }
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
---
+++ POS.Application/Validators/Category/CategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using POS.Application.Dtos.Request;
+
+namespace POS.Application.Validators.Category
+{
+    public class CategoryValidator : AbstractValidator<CategoryRequestDto>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull().WithMessage("El campo no puede ser nulo")
+                .NotEmpty().WithMessage("EL campo nombre no puede ser vacio");
+        }
+    }
+}
